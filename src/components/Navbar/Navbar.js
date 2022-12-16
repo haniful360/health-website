dc85880d34bd3afa4a01e37459ff3806152ede93
Haniful Islam <haniful360@gmail.com>
2022-12-16 12:08:37 +0600

    header and banner section added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import logo from '../../assets/logo-dark.png'
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar border="success" collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <a href="#"> <img src={logo} alt="" /></a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            {/* <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown> */}
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Home</Nav.Link>
+                            <Nav.Link href="#deets">About</Nav.Link>
+                            <Nav.Link href="#deets">Doctor</Nav.Link>
+                            <Nav.Link href="#deets">Department</Nav.Link>
+                            <Nav.Link href="#deets">Contact</Nav.Link>
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <button className='btn btn-primary'>Appointment</button>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </div>
+    );
+};
+
+export default Header;
